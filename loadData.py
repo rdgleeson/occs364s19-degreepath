1fee41ed5ef9750f4715a8f770d1f467cbf30cbf
Jay Messina <jmessin2@oberlin.edu>
2019-05-08 13:24:21 -0400

    requirements in set now

--- loadData.py
+++ loadData.py
@@ -7,33 +7,67 @@ Created on Tue May  7 23:55:52 2019
 """
 
 import pandas
+import os
 
+classes = {}
+
+'''
 def loadReq(directory):
     d = {}
     dataset = pandas.read_excel(directory)
     array = dataset.values
    
     for item in array:
-        if len(item[0])<=22:
+        if len(item[0])<=22 or item[0]=="Statistical Methods (STAT)" or item[0]=="Environmental Studies (ENVS)":
             curKey = item[0]
             d[curKey] = []
         else:
-            d[curKey].append(item)
+            d[curKey].append(item[0])
     return d
+'''
 
+def loadReq(directory):
+    d = set()
+    dataset = pandas.read_excel(directory)
+    array = dataset.values
+   
+    L = ["Quantitative and Formal Reasoning Courses With Prerequisites", "Statistical Methods (STAT)", "Physics and Astronomy (PHYS) (ASTR)", "Environmental Studies (ENVS)", "First Year Seminar Program (FYSP)", 'Computer Science (CSCI)']
+    for item in array:
+        
+        if "-" not in item[0]:
+            continue
+        
+        if len(item[0])<=22 or item[0] in L:
+            continue
+        else:
+            s = item[0].split("-")
+            d.add(s[0][0:len(s[0])-1])
+    return d
     
 def readClasses(directory):
+    
+    #order of labels is: NUMB, SEC, CRN, HRS, MOD, TITLE, DAYS, TIME, BLDG, INSTRUCTOR
     for filename in os.listdir(directory):
+            paren1 = '('
+            paren2 = ')'
+    
             if(filename==".DS_Store"):
                 continue
-            print(filename)
-            file = directory + "/" + filename
+            a = filename.split(" ")
+            #print(a)
+            b = a[len(a)-1].split(".")
+            c = b[0][1:len(b[0])-1]
+            
+            department = c
+            print(department)
+            '''file = directory + "/" + filename
             dataset = pandas.read_excel(file)
             array = dataset.values
             #game = array[:,0,1]
             game = array[:,0:1]
             
             X = array[:,2:17]
+            '''
     
 
 
@@ -42,7 +76,7 @@ def main():
     
     #return dictionary
     directory = "writing_intensive.xlsx"
-    dict_wint = loadWINT(directory)
+    dict_wint = loadReq(directory)
     
     directory = "cultural_diversity.xlsx"
     dict_cd = loadReq(directory)
@@ -53,7 +87,19 @@ def main():
     directory = "QFR.xlsx"
     dict_qfr = loadReq(directory)
     
-    print(dict_qfr)
+    #print(dict_wint)
+    
+   
+    #{"course id exp CSCI 150": course()}
+    directory = "class_schedules_fall_2019"
+    
+    
+    directory = "class_schedules_fall_2018"
+    
+    directory = "class_schedules_spring_2019"
+    readClasses(directory)
+    directory = "class_schedules_spring_2018"
+    
     
     
 main()
