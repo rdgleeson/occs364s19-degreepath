9f6546f61f2cc6fe830ad2ba7f18c3b4dc77728c
Marouane <marouane.abra@gmail.com>
2019-05-09 23:47:12 -0400

    reading in requirements

--- loadData.py
+++ loadData.py
@@ -11,14 +11,14 @@ import os
 
 classes = {}
 
-    
+
 
 '''
 def loadReq(directory):
     d = {}
     dataset = pandas.read_excel(directory)
     array = dataset.values
-   
+
     for item in array:
         if len(item[0])<=22 or item[0]=="Statistical Methods (STAT)" or item[0]=="Environmental Studies (ENVS)":
             curKey = item[0]
@@ -32,72 +32,104 @@ def loadReq(directory):
     d = set()
     dataset = pandas.read_excel(directory)
     array = dataset.values
-   
+
     L = ["Quantitative and Formal Reasoning Courses With Prerequisites", "Statistical Methods (STAT)", "Physics and Astronomy (PHYS) (ASTR)", "Environmental Studies (ENVS)", "First Year Seminar Program (FYSP)", 'Computer Science (CSCI)']
     for item in array:
-        
+
         if "-" not in item[0]:
             continue
-        
+
         if len(item[0])<=22 or item[0] in L:
             continue
         else:
             s = item[0].split("-")
             d.add(s[0][0:len(s[0])-1])
     return d
-    
+
 def readClasses(directory, dict_wint, dict_cd, dict_wadv, dict_qfr):
     global classes
     #order of labels is: NUMB, SEC, CRN, HRS, MOD, TITLE, DAYS, TIME, BLDG, INSTRUCTOR
     for filename in os.listdir(directory):
             paren1 = '('
             paren2 = ')'
-            
+
             if(filename==".DS_Store"):
                 continue
-            
+
             file = directory + "/" + filename
-            
+
             a = filename.split(" ")
             #print(a)
             b = a[len(a)-1].split(".")
             c = b[0][1:len(b[0])-1]
-            
+
             department = c
-            
+
             dataset = pandas.read_excel(file)
             array = dataset.values
             for x in array:
                 #cid, dep, wint, cd, qfr, ch, cat):
                 courseID = department + " " + str(x[0])
                 creditHours = x[3]
-                
+
                 wint = 0
                 cd = 0
                 qfr = 0
-                
+
                 if (courseID in dict_wint or courseID in dict_wadv):
                     wint = 1
                 if (courseID in dict_cd):
                     cd = 1
                 if (courseID in dict_qfr):
                     qfr = 1
-                
+
 
                 value = [courseID, department, wint, cd, qfr, creditHours, ""]
                 classes[courseID] = value
-                
-                
-            
+
+
+
             '''file = directory + "/" + filename
             dataset = pandas.read_excel(file)
             array = dataset.values
             #game = array[:,0,1]
             game = array[:,0:1]
-            
+
             X = array[:,2:17]
             '''
-    
+
+def readMajorRequirements(directory,prog):
+    required_classes = set()
+    electives = set()
+    num_elec_req = 0
+    num_total_req = 0
+    course_dict = {}
+    dataset = pandas.read_excel(directory)
+    array = dataset.values
+    #print(array)
+    for item in array:
+        if num_total_req == 0:
+            if isinstance(item[0],int):
+                num_total_req = item[0]
+                continue
+        if isinstance(item[0],int):
+            num_elec_req = item[0]
+            continue
+        if num_elec_req != 0:
+            electives.add(item[0]+' '+str(int(item[1])))
+        if num_elec_req == 0:
+            required_classes.add(item[0]+' '+str(int(item[1])))
+        if item[3] == "None":
+            course_dict[item[0]+' '+str(int(item[1]))] = []
+        else:
+            course_dict[item[0]+' '+str(int(item[1]))] = [item[3]]
+
+    print("the total number of required classes",num_total_req)
+    print("The prereqs are:",course_dict)
+    print(num_elec_req)
+    print("The electives are:",electives)
+    print("The required classes",required_classes)
+
 
 
 def main():
@@ -106,34 +138,42 @@ def main():
     #return dictionary
     directory = "writing_intensive.xlsx"
     dict_wint = loadReq(directory)
-    
+
     directory = "cultural_diversity.xlsx"
     dict_cd = loadReq(directory)
-    
+
     directory = "writing_advanced.xlsx"
     dict_wadv = loadReq(directory)
-    
+
     directory = "QFR.xlsx"
     dict_qfr = loadReq(directory)
-    
+
     #print(dict_wint)
-    
+
     #{"course id exp CSCI 150": course()}
     directory = "class_schedules_fall_2019"
     readClasses(directory, dict_wint, dict_cd, dict_wadv, dict_qfr)
-    
+
     directory = "class_schedules_fall_2018"
     readClasses(directory, dict_wint, dict_cd, dict_wadv, dict_qfr )
-    
+
     directory = "class_schedules_spring_2019"
     readClasses(directory, dict_wint, dict_cd, dict_wadv, dict_qfr )
-    
+
     directory = "class_schedules_spring_2018"
     readClasses(directory, dict_wint, dict_cd, dict_wadv, dict_qfr )
-    
+
+    #major requirements
+    #cs
+    directory = "csci_major_requirements.xlsx"
+    readMajorRequirements(directory,"csci")
+
+    directory = "csci_minor_requirements.xlsx"
+    readMajorRequirements(directory,"csci")
+
     print()
     global classes
-    print(classes)
-    print(classes["ANTH 353"])
-    
+    #print(classes)
+    #print(classes["ANTH 353"])
+
 main()
